57c61ab03cff85ce6cb59a61ed309787ead3fc00
alcBrains <cazzaitis@live.com>
2022-11-06 20:52:00 +0200

    pagination. fixed column width and other

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,46 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { mergeMap, Subscription, timer } from 'rxjs';
 import { ElectronService } from '../core/services/electron/electron.service';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
-
   @ViewChild('arrivalsPaginator') arrivalsPaginator: MatPaginator;
   @ViewChild('departuresPaginator') departuresPaginator: MatPaginator;
 
+  public PAGE_SIZE = 18;
   subscription = new Subscription();
   arrivals: MatTableDataSource<any>;
   departures: MatTableDataSource<any>;
-  public departuresDisplayedColumns: string[] = ['fnr', 'destination', 'sched', 'schalter', 'terminal', 'gate', 'status']
-  public arrivalsDisplayedColumns: string[] = ['airline', 'fnr', 'codeshare', 'apname', 'sched', 'terminal', 'status']
+  public departuresDisplayedColumns: string[] = [
+    'fnr',
+    'destination',
+    'sched',
+    'schalter',
+    'terminal',
+    'gate',
+    'status',
+  ];
+  public arrivalsDisplayedColumns: string[] = [
+    'airline',
+    'fnr',
+    'codeshare',
+    'apname',
+    'sched',
+    'terminal',
+    'status',
+  ];
 
   constructor(private electronService: ElectronService) {
     this.arrivals = new MatTableDataSource<any>([]);
@@ -27,37 +48,47 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnInit(): void {
-    this.subscription.add(timer(0, 5 * 300000).pipe(mergeMap(() => this.electronService.getArrivals()))
-      .subscribe(data => {
-        const tmp = JSON.parse(data);
-        tmp.forEach(element => {
-          if (element.status.includes('&nbsp;')) {
-            element.status = ''
+    this.subscription.add(
+      timer(0, 5 * 300000)
+        .pipe(mergeMap(() => this.electronService.getDepartures()))
+        .subscribe((data) => {
+          const tmp = JSON.parse(data);
+          tmp.forEach((element) => {
+            if (element.status.includes('&nbsp;')) {
+              element.status = '';
+            }
+          });
+          while (tmp.length % this.PAGE_SIZE != 0) {
+            tmp.push({});
           }
-        });
-        this.arrivals.data = tmp
-      }));
+          this.departures.data = tmp;
+        })
+    );
 
-    this.subscription.add(timer(500, 300000).pipe(mergeMap(() => this.electronService.getDepartures()))
-      .subscribe(data => {
-        const tmp = JSON.parse(data);
-        tmp.forEach(element => {
-          if (element.status.includes('&nbsp;')) {
-            element.status = ''
+    this.subscription.add(
+      timer(0, 5 * 300000)
+        .pipe(mergeMap(() => this.electronService.getArrivals()))
+        .subscribe((data) => {
+          const tmp = JSON.parse(data);
+          tmp.forEach((element) => {
+            if (element.status.includes('&nbsp;')) {
+              element.status = '';
+            }
+          });
+          while (tmp.length % this.PAGE_SIZE != 0) {
+            tmp.push({});
           }
-        });
-        this.departures.data = tmp
-
-      }));
-
+          this.arrivals.data = tmp;
+        })
+    );
   }
 
   ngAfterViewInit(): void {
     this.arrivals.paginator = this.arrivalsPaginator;
     this.departures.paginator = this.departuresPaginator;
     [this.arrivalsPaginator, this.departuresPaginator].forEach((paginator) => {
-      setInterval(() => this.changePage(paginator), 20000)
-    })
+      setInterval(() => this.changePage(paginator), 30000);
+    });
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
@@ -70,5 +101,4 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
       paginator.firstPage();
     }
   }
-
 }
